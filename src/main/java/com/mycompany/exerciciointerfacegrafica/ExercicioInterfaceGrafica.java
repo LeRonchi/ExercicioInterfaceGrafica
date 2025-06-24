d3fb98917eecca6a674e1b008c9e74495f255fbe
LeRonchi <leandrob.ronchi@gmail.com>
2025-06-24 19:30:00 -0300

    git add PainelLogin.java git commit -m "Adiciona PainelLogin com estrutura básica de interface em Java Swing
    Este commit inclui a classe PainelLogin que representa um painel de login com campos de usuário e senha. 
    Foi implementada a interface gráfica usando Java Swing, com objetivo de praticar componentes visuais e layout.
    "

---
+++ src/main/java/com/mycompany/exerciciointerfacegrafica/ExercicioInterfaceGrafica.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.exerciciointerfacegrafica;
+
+/**
+ *
+ * @author LeRonchi
+ */
+public class ExercicioInterfaceGrafica {
+
+    public static void main(String[] args) {
+        
+    }
+}
